b01ffedb8a61897f80803d28564836f5616c0ee7
eddie <energytrain7@gmail.com>
2025-11-12 15:40:22 +0000

    [시각화 프로세서] init 추가

---
+++ llm_engineering/application/visualization/__init__.py
@@ -0,0 +1,12 @@
+"""
+Visualization module for daily activity reports.
+
+일일 활동 데이터를 시각화하는 모듈입니다.
+
+주요 기능:
+- Interactive Plotly 시각화 (daily_report_interactive.py)
+- Streamlit 대시보드 (streamlit_daily_report.py)
+
+사용법:
+    poe daily-report
+"""
